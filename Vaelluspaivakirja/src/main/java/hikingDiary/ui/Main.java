33fddd13f7fcdfe6697a9a01d1128e950aeab347
Veera Lupunen <veera.lupunen@helsinki.fi>
2020-03-31 22:06:59 +0300

    Tekstikäyttöliittymään palaaminen

--- Vaelluspaivakirja/src/main/java/hikingDiary/ui/Main.java
+++ Vaelluspaivakirja/src/main/java/hikingDiary/ui/Main.java
@@ -13,13 +13,13 @@ import javafx.application.Application;
  */
 public class Main {
     public static void main(String[] args) {
-        Application.launch(GraphicalUserInterface.class);
-//        Scanner reader = new Scanner(System.in);
-//        System.out.println("Hello there! What is your name?");
-//        String name = reader.nextLine();
-//        User u = new User(name);
-//        UserInterface ui = new UserInterface(reader, u);
-//        ui.start();
+//        Application.launch(GraphicalUserInterface.class);
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Hello there! What is your name?");
+        String name = reader.nextLine();
+        User u = new User(name);
+        UserInterface ui = new UserInterface(reader, u);
+        ui.start();
         
     }
 }
